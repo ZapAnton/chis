d289d9440aaf854cec214ec1c71abf3f19ea3c19
ZapAnton <wc3ft2@gmail.com>
2020-08-03 15:48:58 +0300

    Added passing the game file as a command line argument

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Initial" << std::endl;
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Please, provide a path to the CHIP-8 game file" << std::endl;
+        return -1;
+    }
+    std::cout << "Opening game file " << argv[1] << std::endl;
     return 0;
 }
